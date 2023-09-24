634303e200ac1863ce12029be1cc74d689181677
zerocl460 <125226411+zerocl460@users.noreply.github.com>
2023-09-23 20:10:27 -0400

    Feature: Add folder-specific renaming and image processing script
    Description:
    This commit adds a PowerShell script that performs a series of operations on images within a selected folder. The script takes into account the folder name for file renaming.
    
    Major Changes:
    
    Implement folder selection through a dialog.
    Add capability to copy original files into a folder named Original.
    Create a function to randomize, rename, and renumber files into a folder named Original-Renum. The renaming is based on a formatted version of the folder name.
    Add a function for color correction of files using ImageMagick and store them in a folder named Original-CC.
    Additional Information:
    
    The renumbered/renamed filenames are generated using a substring of the folder name, converted to PascalCase.

--- image_processing.ps1
+++ image_processing.ps1
@@ -1,38 +1,63 @@
-# Load Windows Forms assembly
+# Load Windows Forms assembly for folder picker
 Add-Type -AssemblyName System.Windows.Forms
 
-# Initialize Folder Browser Dialog
-$folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-$folderBrowser.Description = "Select a Folder"
-
-# Show Folder Browser Dialog and get the folder path
-if ($folderBrowser.ShowDialog() -eq "OK") {
-    $folderPath = $folderBrowser.SelectedPath
-} else {
-    Write-Host "No folder selected. Exiting."
-    exit
+# Function to pick a folder using a dialog
+Function Select-Folder {
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderBrowser.ShowDialog() | Out-Null
+    return $folderBrowser.SelectedPath
 }
 
-# Change directory to selected folder
-Set-Location -Path $folderPath
-
-# Get the name of the current directory (your folder name)
-$foldername = Split-Path -Leaf (Get-Location)
-
-# Initialize a counter for numbering
-$counter = 0
-
-# Loop through all JPEG files in the current directory
-Get-ChildItem *.jpg | ForEach-Object {
-
-    # Create an output filename based on the folder name and counter
-    $outputFilename = "$foldername" + "_$(("{0:D4}" -f $counter)).jpg"
-
-    # Run ImageMagick command to adjust image
-    & 'magick' $_.Name -gamma 2 -modulate 120 -auto-level $outputFilename
-
-    # Increment the counter
-    $counter++
+# Function to copy original files to 'Original' folder
+Function Copy-Original-Files ($folderPath) {
+    $originalFolder = Join-Path $folderPath "Original"
+    New-Item -ItemType Directory -Path $originalFolder -Force
+    Copy-Item "$folderPath\*.png" -Destination $originalFolder
 }
 
-Write-Host "Image processing complete."
+# Function to copy and renumber files to 'Original-Renum' folder
+Function Copy-Renum-Files ($folderPath, $folderName) {
+    $renumFolder = Join-Path $folderPath "Original-Renum"
+    New-Item -ItemType Directory -Path $renumFolder -Force
+
+    # Extract the part of the folder name after the '-'
+    $relevantName = ($folderName -split ' - ')[1]
+
+    # Convert the extracted name to PascalCase (aka UpperCamelCase)
+    $formattedName = ($relevantName -split ' ' | ForEach-Object { $_.Substring(0, 1).ToUpper() + $_.Substring(1).ToLower() }) -join ''
+
+    $counter = 1
+    Get-ChildItem "$folderPath\Original\*.png" | Get-Random -Count (Get-ChildItem "$folderPath\Original\*.png").Count | ForEach-Object {
+        $newName = "${formattedName}_$(("{0:D3}" -f $counter)).png"
+        Copy-Item $_.FullName -Destination (Join-Path $renumFolder $newName)
+        $counter++
+    }
+}
+
+
+# Function to color-correct files and save to 'Original-CC' folder
+Function Color-Correct-Files ($folderPath) {
+    $ccFolder = Join-Path $folderPath "Original-CC"
+    New-Item -ItemType Directory -Path $ccFolder -Force
+    Get-ChildItem "$folderPath\Original-Renum\*.png" | ForEach-Object {
+        $outputFilename = Join-Path $ccFolder $_.Name
+        & 'magick' $_.FullName -depth 16 -modulate 100,140,100 -auto-level -brightness-contrast 10x10 -colorspace sRGB -enhance -contrast -contrast-stretch 0 $outputFilename
+    }
+}
+
+# Main Script Execution Starts Here
+
+# Step 1: Select the working folder
+$folderPath = Select-Folder
+$folderName = (Get-Item -Path $folderPath).Name
+
+# Step 2: Copy original files to 'Original' folder
+Copy-Original-Files -folderPath $folderPath
+
+# Step 3: Copy and renumber files to 'Original-Renum' folder
+Copy-Renum-Files -folderPath $folderPath -folderName $folderName
+
+# Step 4: Perform color correction and save to 'Original-CC' folder
+Color-Correct-Files -folderPath $folderPath
+
+Write-Host "All operations complete."
